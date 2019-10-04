abec27aa062f283377886c49c4f8fb5f4e53962f
Joe Manto <joe.m.manto@wmich.edu>
2019-10-04 17:52:19 -0400

    work on test cases

--- testing/test.js
+++ testing/test.js
@@ -32,33 +32,68 @@
  const RecordService = require('../Server/RecordService.js');
  let records_service = new RecordService();
 
+class Example {
+  testFunction1(arr){
+    if(arr.length > 5){
+      return true;
+    }
+    return false;
+  }
+
+  testFunction2(str){
+    for(let i = 0;i<str.length; i++){
+      if(str[i] === '%'){
+        str[i] = ' ';
+      }
+    }
+  }
+}
+
 
  class TestCase {
-   constructor(classPointer,functionPointer,expectedResult) {
+   constructor(classPointer,functionPointer,inputs,expectedResult) {
      this.classPointer = classPointer;
      this.functionPointer = functionPointer;
+     this.inputs = inputs;
      this.expectedResult = expectedResult;
    }
    runTest(){
-     let output = this.classPointer[this.functionPointer]();
-     let result = {status:false};
+     for(let i = 0; i<this.inputs.length; i++){
+       let output;
+       let result;
 
-     if(typeof this.expectedResult === 'function'){
        try{
-          result = {status:this.expectedResult()};
+         output = this.classPointer[this.functionPointer](this.inputs[i]);
        }catch(error){
-          result = {status:false,error:error}
+         result = {status:false,error:error}
+         this.printTestResult(result,this.inputs[i]);
+         continue;
        }
-     }else{
-       let result = {status:output === this.expectedResult};
-     }
 
-     if()
+       if(typeof this.expectedResult === 'function'){
+          result = {status:output === this.expectedResult(this.inputs[i],output),output:output};
+
+       }else{
+          result = {status:output === this.expectedResult[i],output:output};
+       }
+       this.printTestResult(result,this.inputs[i]);
+     }
    }
 
-
-
+   printTestResult(result,input){
+     if(result.error){
+       console.log("..........⛔️ " + this.functionPointer + " Caused an error with input ["+input+"]")
+       return;
+     }
+     if(result.status){
+       console.log("..........✅ " + this.functionPointer + " passed with input ["+input+ "] and output "+result.output);
+     }else{
+       console.log("..........❌" + this.functionPointer + " Failed with input ["+input+ "] and output "+result.output);
+     }
+   }
  }
 
-let result = new TestCase(records_service,"checkForDataMigration",()=>{return true});
-result.runTest();
+let example = new Example();
+let test1 = new TestCase(example,"testFunction1",[[1,2,3,4,5,6],[1,3,5,6]],[true,false]);
+let test2 = new TestCase(example,"testFunction2",["hello%joe","what%%test"],(input,output) => {return (input.replace('%',' ') === output)});
+test2.runTest();
