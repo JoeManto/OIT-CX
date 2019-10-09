dd139db0f57a8b5bb3315ced7c058fd5f1e0a67b
Joe Manto <joe.m.manto@wmich.edu>
2019-10-09 14:51:29 -0400

    work on testing framework

--- testing/test.js
+++ testing/test.js
@@ -43,9 +43,14 @@ class Example {
   testFunction2(str){
     for(let i = 0;i<str.length; i++){
       if(str[i] === '%'){
-        str[i] = ' ';
+
       }
     }
+    return str;
+  }
+
+  testFunction3(input){
+    return input;
   }
 }
 
@@ -61,7 +66,6 @@ class Example {
      for(let i = 0; i<this.inputs.length; i++){
        let output;
        let result;
-
        try{
          output = this.classPointer[this.functionPointer](this.inputs[i]);
        }catch(error){
@@ -70,9 +74,19 @@ class Example {
          continue;
        }
 
-       if(typeof this.expectedResult === 'function'){
-          result = {status:output === this.expectedResult(this.inputs[i],output),output:output};
+       console.log(typeof this.expectedResult);
 
+       if(typeof this.expectedResult === 'function'){
+          result = {status:this.expectedResult(this.inputs[i],output),output:output}
+       }else if(typeof this.expectedResult[i] === 'object'){
+          let keys = Object.keys(this.expectedResult[i]);
+          result = {status:true,output:output};
+
+          for(let key in keys){
+            console.log(keys[key]);
+            if(this.expectedResult[keys[key]] !== output[keys[key]])
+              result = {status:false};
+          }
        }else{
           result = {status:output === this.expectedResult[i],output:output};
        }
@@ -95,5 +109,6 @@ class Example {
 
 let example = new Example();
 let test1 = new TestCase(example,"testFunction1",[[1,2,3,4,5,6],[1,3,5,6]],[true,false]);
-let test2 = new TestCase(example,"testFunction2",["hello%joe","what%%test"],(input,output) => {return (input.replace('%',' ') === output)});
-test2.runTest();
+let test2 = new TestCase(example,"testFunction2",["hello%joe","what%%test"],(input,output) => {return true});
+let test3 = new TestCase(example,"testFunction3",[{name:"hi",number:5}],[{name:"hi",number:5}]);
+test3.runTest();
