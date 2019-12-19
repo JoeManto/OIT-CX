743fad07efce183b9b29d13d21bc0ecb613ec1b8
JoeManto <joemanto123@gmail.com>
2019-12-19 16:50:27 -0500

    fix data migration bug and added a test case for it

--- Util/Util.js
+++ Util/Util.js
@@ -3,4 +3,8 @@ const sleep = (milliseconds) => {
   return new Promise(resolve => setTimeout(resolve, milliseconds))
 }
 
-module.exports = {sleep:sleep};
+const dateToMysqlDateTime = (date) => {
+  return date.toISOString().slice(0, 19).replace('T', ' ');
+}
+
+module.exports = {sleep:sleep,dateToMysqlDateTime:dateToMysqlDateTime};
