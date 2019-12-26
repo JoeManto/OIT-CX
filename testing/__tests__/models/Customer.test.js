2b62411be1cd02b0709cf320c13422b83f29e0ae
JoeManto <joemanto123@gmail.com>
2019-12-25 23:06:54 -0500

    added tests and refactoring to the customer class

--- testing/__tests__/models/Customer.test.js
+++ testing/__tests__/models/Customer.test.js
@@ -1,6 +1,41 @@
-describe('create customer function', () => {
+const dbhandler = require('../../../Server/wrappers/MysqlWrapper');
+const Customer = require('../../../Server/models/Customer');
 
-    it('Should ',() => {
+afterAll(()=>{
+    dbhandler.db.destroy();
+})
+
+
+describe('Customer Functions', () => {
+
+
+
+
+    it('Should apply/find customer data that is not in the db (vaild bnid)',async() => {
+
+      //clean
+      dbhandler.query("delete from customer where bnid = ?",{conditions:['jfj5666']});
+      let customer = new Customer();
+
+      //
+      let data = await customer.apply('jfj5666');
+
+      let condition = !(data instanceof Error);
+      expect(condition).toBeTruthy();
+    });
+
+    it('Should fail to apply/find customer data that is unknown to ldap', async() => {
+
+      //clean
+      dbhandler.query("delete from customer where bnid = ?",{conditions:['dfs']});
+      let customer = new Customer();
+
+      //create an unknown user
+      let condition = await customer.apply('dfs')
+      .then(()=>true)
+      .catch(()=>false);
+
+      expect(condition).toBeFalsy();
 
     });
 })
