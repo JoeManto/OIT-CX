eb7aee913f78f5acfbe5abbb90c44cde0c487423
JoeManto <joemanto123@gmail.com>
2020-01-19 20:00:50 -0500

    changed the user lookup function can now search in any db column

---
+++ Server/models/Shift.js
@@ -0,0 +1,26 @@
+const db = require('../wrappers/MysqlWrapper');
+
+/*
+ Shift Abstraction
+ -  Find Shifts from id
+ -
+*/
+
+class Shift {
+    constructor(shiftID){
+        this.shiftID = shiftID;
+        this.shiftData = {
+
+        }
+    }
+
+    /*
+
+    */
+    apply(shiftID){
+
+    }
+
+
+
+}
