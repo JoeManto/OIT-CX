2ae6a8064764afb0bbe5c560d0b1484b7881c97c
unknown <joemanto123@gmail.com>
2020-01-24 21:00:32 -0500

    added tests for User class

--- testing/__tests__/models/User.test.js
+++ testing/__tests__/models/User.test.js
@@ -11,11 +11,26 @@ describe('lookUp Function',() => {
 
         let user = new User(undefined,'employee');
         //Assumes the database has user with an id of 3
-        let data = await user.lookup({by:'id',value:'3'})
-        .catch(err => console.log(err));
+        let data = await user.lookup({by:'id',value:'3'});
 
         expect(data).not.toBe(undefined);
+    });
 
+    it('Should find employee records by directly setting bnid',async() => {
+        let user = new User('jfj5666','employee');
+
+        let data = await user.lookup({});
+
+        expect(data).not.toBe(undefined);
+    });
+
+    it('Should find customer records by id', () => {
+        let customer = new User(undefined,'customer');
+
+        //Assumes Data customer with id 686 exists
+        let data = await user.lookup({by:'id',value:'686'});
+
+        expect(data).not.toBe(undefined);
     });
 });
 
