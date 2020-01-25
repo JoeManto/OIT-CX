d3dd54d855107f31765cc88dfabee74a9a09aed7
unknown <joemanto123@gmail.com>
2020-01-25 18:10:45 -0500

    added tests for shfit deletion and migration and expired shifts

--- testing/__tests__/models/ShiftService.test.js
+++ testing/__tests__/models/ShiftService.test.js
@@ -33,36 +33,86 @@ const insertTestExpiredShift = async() => {
     await new Shift().create(shiftData);
 }
 
-const insertTestValidShift = () => {
+const insertTestValidShift = async() => {
     let today = new Date();
     let end = new Date();
-    end.setDate(today + 1);
+    end.setDate(today.getDate() + 1);
 
     let shiftData = {...data,
         shiftDateEnd:end,
         shiftDateStart:today,
     }
 
-    new Shift().create(shiftData);
+    await new Shift().create(shiftData);
 }
 
-flush = async(table) => {
-    await dbhandler.query('delete from '+table+' where id >= 0');
+flushShifts = async(table) => {
+    await dbhandler.query('delete from '+table+' where shiftID >= 0');
 }
 
-let service = new ShiftService();
-
-describe('Shift Migration', () => {
-    it('Should delete an expired shift', async() => {
+describe('Shift Deletion', () => {
+    it('Should un-track,delete and migrate an expired shift', async() => {
         
-        //flush('legacyshifts');
-        //flush('shifts');
+        flushShifts('legacyshifts');
+        flushShifts('shifts');
 
-        insertTestExpiredShift();
-        
-      
+        await insertTestExpiredShift();
 
-        console.log(service.openShifts);
+        let service = new ShiftService();
         
+        //capture expired shift
+        await service.gatherOpenShifts();
+
+        expect(service.openShifts.length).toBe(1);
+
+        //find expired shift then delete and migrate
+        let deletionSyncWrapper = async() => {service.search();}
+
+        await deletionSyncWrapper();
+
+        //Expect the shift to be no longer being tracked
+        expect(service.openShifts.length).toBe(0);
+
+        let resolves = await Promise.all([
+            dbhandler.query('select * from shifts'),
+            dbhandler.query('select * from legacyshifts'),
+        ]);
+
+        resolves = resolves.map((obj) => obj.length);
+        
+        expect(resolves).toEqual([0,1]);
+    });
+
+    it.only('Should NOT un-track,delete and migrate a valid non-expired shift', async() => {
+        
+        flushShifts('legacyshifts');
+        flushShifts('shifts');
+
+        await insertTestValidShift();
+
+        let service = new ShiftService();
+
+        //capture valid shift
+        await service.gatherOpenShifts();
+
+        expect(service.openShifts.length).toBe(1);
+
+        //find expired shift then delete and migrate
+        let deletionSyncWrapper = async() => {service.search();}
+
+        await deletionSyncWrapper();
+
+        await service.gatherOpenShifts();
+
+        expect(service.openShifts.length).toBe(1);
+
+        let resolves = await Promise.all([
+            dbhandler.query('select * from shifts'),
+            dbhandler.query('select * from legacyshifts'),
+        ]);
+
+        resolves = resolves.map((obj) => obj.length);
+        
+        expect(resolves).toEqual([1,0]);
     });
 });
