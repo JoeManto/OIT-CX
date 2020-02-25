f8cbd973ebb4196ca4f3d43fb743d6d2a88ce5d7
Joe Manto <joemanto123@gmail.com>
2020-02-25 15:23:20 -0500

    updated the change log

--- src/js/ChangeLog.js
+++ src/js/ChangeLog.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import '../css/ChangeLog.css';
 import emailChangeImage from '../update-log-res/update-screenshot-01.png';
+import postShiftButtonImage from '../update-log-res/update-screenshot-02.png';
+import dayOfTheWeekShifts from '../update-log-res/update-screenshot-03.png';
+
 
 export default class ChangeLog extends React.Component {
     render(){
@@ -9,10 +12,8 @@ export default class ChangeLog extends React.Component {
                 <h1>Change Log for OIT-CX</h1>
                 <UpdateHeader date = {'02/15/2020'} updateNumber = {'1'}/>
                 <ChangesContainer>
-                    <ChangeElement title = {'Fixed Posting Shift Warning Bugs'} content = {`
-                        Long shift time and over night warnings were being displayed when using preset shifts like (mobile and call-in) 
-                    `}/>
-                    
+
+                    <SubHeader text = {'Emails'}/>
                     <ChangeElement title = {'Emails for Shifts being covered'} content = {
                     `
                         Emails will now be sent to the shift requester and the person who picked up a shift.   
@@ -20,11 +21,43 @@ export default class ChangeLog extends React.Component {
                     }
                     image = {emailChangeImage}/>
 
+                    <ChangeElement title = {'Emails and permanent shifts'} content = {
+                    `
+                        There is now a item in the details section of the shift email notifications that show if a shift is permanent or not.
+                    `
+                    }/>
+                    <SubHeader text = {'Shifts'}/>
+                    <ChangeElement title = {'Shift cards now display the day of the week'} content = {
+                    `
+                        For normal shifts (open and picked up) the day of the week will be displayed before the numerical date.
+                        For permanent shifts the day of the week is the only information displayed in the title.
+                    `
+                    } image = {dayOfTheWeekShifts}/>
+
                     <ChangeElement title = {'Fixed a crash on the post shift page'} content = {
                     `
                         Hovering over the footer will no longer crash the page.
                     `
                     }/>
+
+                    <ChangeElement title = {'Fixed Posting Shift Warnings'} content = {`
+                        Long shift time and over night warnings were being displayed when using preset shifts like (mobile and call-in) 
+                    `}/>
+
+                    <ChangeElement title = {'Changed the highlight color of shifts'} content = {
+                    `
+                        The highlight color when hovering over a shift in the shifts page is now a darker appearance rather than a lighter appearance.
+                    `
+                    }/>
+
+                    <ChangeElement title = {'Changed the post shift button'} content = {
+                    `
+                        The post shift button now actually looks like a button.
+                    `
+                    } image = {postShiftButtonImage}/>
+
+
+                    <SubHeader text = {'Other Changes and Bug Fixes'}/>
 
                     <ChangeElement title = {'Fixed the text color for input text containers'} content = {
                     `
@@ -32,18 +65,16 @@ export default class ChangeLog extends React.Component {
                     `
                     }/>
 
-                    
-                    <ChangeElement title = {'Changed the highlight color of shifts'} content = {
-                    `
-                        The highlight color when hovering over a shift in the shifts page is now a darker appearance rather than a lighter appearance.
-                    `
-                    }/>
+                
+  
 
                     <ChangeElement title = {'Changed the mobile header background'} content = {
                     `
                         Mobile page headers now reflect the dark theme.
                     `
                     }/>
+    
+        
 
                     <ChangeElement title = {'Back-End'} content = {
                     `
@@ -87,6 +118,15 @@ function UpdateHeader(props){
             </div>
         </div>
     ); 
+}
+
+function SubHeader(props){
+    const style = {
+        color:'#ffcc33'
+    }
+    return(
+        <h2 style = {style}>{props.text}</h2>
+    );
 }
 
 function ChangeElement(props){
