2bc902ff4b35d9a53307efc038a918e2d4ef20b5
Joe Manto <joemanto123@gmail.com>
2020-03-06 18:43:09 -0500

    added template operation component to the admin page

--- src/js/AdminPage.js
+++ src/js/AdminPage.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import '../css/AdminPage.css';
 import '../css/util.css';
+import '../css/OperationTutorialView.css';
+import {WTInputPanelController,WTInputPanel,Input1,SelectionController} from './components/OperationTutorialView'
 
  export default class AdminPage extends React.Component {
     constructor(props){
@@ -9,7 +11,43 @@ import '../css/util.css';
 
     render(){
         return(
-            <div></div>
+			
+            <div className = "admin-operations-list-cnt">
+				<WTInputPanelController title = {'Add User'}
+				 description = {`
+				 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.	
+				`} 
+				numPanels={4}>
+                    <WTInputPanel
+                    title={"Bronco Net-ID"}
+                    subtitle={"Western Michigan University ID"}
+                    >
+                        <Input1 title={"Bnid"} />
+                    </WTInputPanel>
+
+                    <WTInputPanel 
+                    title={"Department"} 
+                    subtitle={"OIT Department"}>
+                        <SelectionController
+							open = {true}
+                            fields={["Help-Desk", "Class Tech", "Operators"]}
+                        />
+                    </WTInputPanel>
+
+                    <WTInputPanel 
+                    title={"Perfered Name"}
+                    subtitle={"IDK A NAME"}>
+                        <Input1 title={"Name"} />
+                    </WTInputPanel>
+
+                    <WTInputPanel
+                    title={"User Role"}
+                    subtitle={"Assignment of permissions"}
+                    >
+                        <SelectionController open = {true} fields={["Normal", "Supervisor"]} />
+                    </WTInputPanel>
+                </WTInputPanelController>
+            </div>
         );
     }
 }
