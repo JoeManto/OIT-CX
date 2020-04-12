0d3775f6e0985c26440933404155951f854a6f5f
Joe Manto <joemanto123@gmail.com>
2020-04-11 20:05:20 -0400

    added automatic init db for dbwait-script

--- dbwait-script.sh
+++ dbwait-script.sh
@@ -1,18 +1,27 @@
-mysqlRootPass=root;
+mysqlRootPass=12345678;
+host=localhost;
 
-while ! mysql -h db -u root -p$mysqlRootPass -P 3306 -e "SELECT VERSION();SELECT NOW()"; do
+while ! mysql -h $host -u root -p$mysqlRootPass -P 3306 -e "SELECT VERSION();SELECT NOW()"; do
     sleep 5;
     echo "mysql is not connected";
 
 done;
 
-#exec mysql -h db -u root -p$mysqlRootPass -P 3306 -e "CREATE DATABASE nodemysql"
-#exec mysql -h db -u root -p$mysqlRootPass -P 3306 nodemysql < db_init.sql
+#mysql -h $host -u root -p$mysqlRootPass -P 3306 -e 'DROP DATABASE nodemysql';
 
-mysql -h db -u root -p$mysqlRootPass -P 3306 -e "USE nodemysql;create table env (id int PRIMARY KEY AUTO_INCREMENT,varType varchar(255), varName varchar(255), varValue varchar(255)); insert into env (varType,varName,varValue) values ('string','LDAP_PASS','testpassword')";
+databases="$(mysql -h $host -u root -p$mysqlRootPass -P 3306 -e 'show databases')";
+
+if [[ "$databases" != *"nodemysql"* ]]; then
+  echo "database wasn't found starting init of fresh db";
+
+  mysql -h $host -u root -p$mysqlRootPass -P 3306 -e 'CREATE DATABASE nodemysql';
+  mysql -h $host -u root -p$mysqlRootPass -P 3306 nodemysql < db_init.sql
+  mysql -h $host -u root -p$mysqlRootPass -P 3306 -e "USE nodemysql;create table env (id int PRIMARY KEY AUTO_INCREMENT,varType varchar(255), varName varchar(255), varValue varchar(255));"
+
+fi
 
 #override process enviorment vars
-overrides="$(mysql -h db -u root -p$mysqlRootPass -P 3306 -e 'USE nodemysql;select * from env')";
+overrides="$(mysql -h $host -u root -p$mysqlRootPass -P 3306 -e 'USE nodemysql;select * from env')";
 
 gcc env_override_parser.c;
 
@@ -21,11 +30,11 @@ parsed_overrides="$(./a.out $overrides)";
 split=( $( echo $parsed_overrides | cut -d' ' -f1- ) );
 
 for ((i = 0; i < ${#split[@]}; i++)); do 
+    echo 'exporting';
     export ${split[$i]};
 done
 
 
-
 #Node entry
 echo "starting node";
-exec node Server/server.js
+#exec node Server/server.js
